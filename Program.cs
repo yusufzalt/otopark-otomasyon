47df7c4bf88412feeb01964136e36423040e3083
yusufzalt <62478972+yusufzalt@users.noreply.github.com>
2020-03-21 20:51:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApplication1
+
+
+/****************************************************************************
+** SAKARYA ÜNİVERSİTESİ
+** BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+** BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ
+** NESNEYE DAYALI PROGRAMLAMA DERSİ
+** 2019-2020 BAHAR DÖNEMİ
+**
+** ÖDEV NUMARASI : 1
+** ÖĞRENCİ ADI :yusuf zalt
+** ÖĞRENCİ NUMARASI : b161200556
+** DERSİN ALINDIĞI GRUP : A
+****************************************************************************/
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            ArrayList araclar = new ArrayList();
+            int sayi = 0;
+        menu:
+            Console.WriteLine("");
+            Console.WriteLine("1-Araç Giriş");
+            Console.WriteLine("2-Araç Çıkış");
+            Console.WriteLine("3-Program Çıkış");
+            Console.WriteLine("Seçiminiz.....");
+            Console.WriteLine("");
+            string secim = Console.ReadLine();
+            if (secim == "1")
+            {
+                if (sayi == 20)
+                {
+                    Console.WriteLine("Otopark Dolu İlk olarak Arç Çıkışı Yapın");
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                    goto menu;
+                }
+                Console.WriteLine("Araç Plakası");
+                araclar.Add(Console.ReadLine());
+                Console.WriteLine("");
+                sayi++;
+                goto menu;
+            }
+            if (secim == "2")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Çıkacak Araç Plakası");
+                string cikacak = Console.ReadLine();
+                int sira = araclar.IndexOf(cikacak);
+                if (sira >= 0)
+                {
+                    Console.WriteLine("Kaç Saat Kaldı");
+                    int saat = Int16.Parse(Console.ReadLine());
+
+                    if (saat == 1)
+                    {
+                        Console.WriteLine("Ödenecek Ücret 5TL");
+                    }
+                    if (saat == 2)
+                    {
+                        Console.WriteLine("Ödenecek Ücret 10TL");
+                    }
+                    if (saat == 3)
+                    {
+                        Console.WriteLine("Ödenecek Ücret 15TL");
+                    }
+                    if (saat > 3)
+                    {
+                        Console.WriteLine("Ödenecek Ücret 40TL");
+                    }
+                    Console.WriteLine("");
+                    sayi--;
+                    araclar.RemoveAt(sira);
+                }
+                goto menu;
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
